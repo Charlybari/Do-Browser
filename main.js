c3cd33d2385ea82eef556111154d020b4305e651
Charlybari <143527684+Charlybari@users.noreply.github.com>
2024-12-14 16:54:44 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+// A simple browser implementation using Electron
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL('https://www.google.com');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
